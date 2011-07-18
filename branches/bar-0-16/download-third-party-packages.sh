f135028544b574b37863694e57b34efbe8144d2f
trupp <trupp@9c91cb43-a425-4971-9156-864750d2c3cf>
2011-07-18 19:03:18 +0000

    fix FTP patches

--- branches/bar-0-16/download-third-party-packages.sh
+++ branches/bar-0-16/download-third-party-packages.sh
@@ -279,9 +279,7 @@ if test $cleanFlag -eq 0; then
      fi
      if test $noDecompressFlag -eq 0; then
        $TAR xzf ftplib-3.1-src.tar.gz
-       (cd ftplib-3.1; patch -p3 < ../ftplib-3.1-1.patch)
-       (cd ftplib-3.1; patch -p1 < ../../misc/ftplib-3.1-ftpaccess.patch)
-       (cd ftplib-3.1; patch -p1 < ../../misc/ftplib-3.1-receive-timeout.patch)
+       (cd ftplib-3.1; patch -p3 < ../ftplib-3.1-1.patch) 1>/dev/null 2>/dev/null
      fi
     )
     if test $noDecompressFlag -eq 0; then
